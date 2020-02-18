89409b8dba136b82cd26ecadd4bf29b5168f5946
Chenhui <chenhhu@microsoft.com>
2020-02-18 21:54:06 +0000

    changed yaml to yml

--- tools/environment_setup.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-
-conda update conda
-conda env create -f tools/environment.yaml
-eval "$(conda shell.bash hook)" && conda activate forecasting_env
-pip install -e fclib
-python -m ipykernel install --user --name forecasting_env
--- tools/environment_setup.sh
+++ tools/environment_setup.sh
@@ -3,7 +3,7 @@
 
 
 conda update conda
-conda env create -f tools/environment.yaml
+conda env create -f tools/environment.yml
 eval "$(conda shell.bash hook)" && conda activate forecasting_env
 pip install -e fclib
 python -m ipykernel install --user --name forecasting_env
