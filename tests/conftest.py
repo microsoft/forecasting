0aa57c1f86138e53c46d2f73bd54a8dea2170c55
Chenhui Hu <chenhhu@microsoft.com>
2020-02-18 18:15:36 +0000

    changed import statement in confest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import os
 import pytest
-from fclib.common.utils import git_repo_path
+#from fclib.common.utils import git_repo_path
 
 
 @pytest.fixture(scope="module")
 def notebooks():
+    from fclib.common.utils import git_repo_path
     repo_path = git_repo_path()
     examples_path = os.path.join(repo_path, "examples")
     quick_start_path = os.path.join(examples_path, "00_quick_start")
