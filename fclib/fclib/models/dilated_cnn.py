691f094964fc93c582a6fb4a7396740b2619521a
Ubuntu <chenhhu@microsoft.com>
2020-02-27 04:02:28 +0000

    updated model utils

--- fclib/fclib/models/dilated_cnn.py
+++ fclib/fclib/models/dilated_cnn.py
@@ -7,14 +7,22 @@ solve time series forecasting problems.
 """
 
 
+from math import ceil, log
 from tensorflow.keras.layers import Input, Lambda, Embedding, Conv1D, Dropout, Flatten, Dense, concatenate
 from tensorflow.keras.models import Model
 
-MAX_STORE_ID = 137
-MAX_BRAND_ID = 11
 
-
-def create_dcnn_model(seq_len, kernel_size=2, n_filters=3, n_input_series=1, n_outputs=1, dropout_rate=0.1):
+def create_dcnn_model(
+    seq_len,
+    n_dyn_fea=1,
+    n_cat_fea=2,
+    n_outputs=1,
+    kernel_size=2,
+    n_filters=3,
+    dropout_rate=0.1,
+    max_grain1_id=1e3,
+    max_grain2_id=1e3,
+):
     """Create a Dilated CNN model.
 
     Args: 
@@ -24,17 +32,17 @@ def create_dcnn_model(seq_len, kernel_size=2, n_filters=3, n_input_series=1, n_o
         n_outputs (Integer): Number of outputs in the last layer
 
     Returns:
-        Keras Model object
+        object: Keras Model object
     """
     # Sequential input
-    seq_in = Input(shape=(seq_len, n_input_series))
+    seq_in = Input(shape=(seq_len, n_dyn_fea))
 
     # Categorical input
-    cat_fea_in = Input(shape=(2,), dtype="uint8")
+    cat_fea_in = Input(shape=(n_cat_fea,), dtype="uint8")
     store_id = Lambda(lambda x: x[:, 0, None])(cat_fea_in)
     brand_id = Lambda(lambda x: x[:, 1, None])(cat_fea_in)
-    store_embed = Embedding(MAX_STORE_ID + 1, 7, input_length=1)(store_id)
-    brand_embed = Embedding(MAX_BRAND_ID + 1, 4, input_length=1)(brand_id)
+    store_embed = Embedding(max_grain1_id + 1, ceil(log(max_grain1_id + 1)), input_length=1)(store_id)
+    brand_embed = Embedding(max_grain2_id + 1, ceil(log(max_grain2_id + 1)), input_length=1)(brand_id)
 
     # Dilated convolutional layers
     c1 = Conv1D(filters=n_filters, kernel_size=kernel_size, dilation_rate=1, padding="causal", activation="relu")(
