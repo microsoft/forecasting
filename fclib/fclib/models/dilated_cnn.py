b3e35c5dd27a9935e571709f3ca967a2df1e94c4
Ubuntu <chenhhu@microsoft.com>
2020-02-27 18:26:09 +0000

    generalized categorical features in dcnn model util

--- fclib/fclib/models/dilated_cnn.py
+++ fclib/fclib/models/dilated_cnn.py
@@ -13,36 +13,33 @@ from tensorflow.keras.models import Model
 
 
 def create_dcnn_model(
-    seq_len,
-    n_dyn_fea=1,
-    n_cat_fea=2,
-    n_outputs=1,
-    kernel_size=2,
-    n_filters=3,
-    dropout_rate=0.1,
-    max_grain1_id=1e3,
-    max_grain2_id=1e3,
+    seq_len, n_dyn_fea=1, n_cat_fea=2, n_outputs=1, kernel_size=2, n_filters=3, dropout_rate=0.1, max_cat_id=[1e3, 1e3],
 ):
     """Create a Dilated CNN model.
 
     Args: 
-        seq_len (Integer): Input sequence length
-        kernel_size (Integer): Kernel size of each convolutional layer
-        n_filters (Integer): Number of filters in each convolutional layer
-        n_outputs (Integer): Number of outputs in the last layer
+        seq_len (int): Input sequence length
+        n_dyn_fea (int): Number of dynamic features
+        n_cat_fea (int): Number of categorical features
+        n_outputs (int): Number of outputs of the network
+        kernel_size (int): Kernel size of each convolutional layer
+        n_filters (int): Number of filters in each convolutional layer
+        dropout_rate (float): Dropout rate in the network
+        max_cat_id (list[int]): Maximum value of the grain1 ID
 
     Returns:
         object: Keras Model object
     """
-    # Sequential input
+    # Sequential input for dynamic features
     seq_in = Input(shape=(seq_len, n_dyn_fea))
 
     # Categorical input
     cat_fea_in = Input(shape=(n_cat_fea,), dtype="uint8")
-    store_id = Lambda(lambda x: x[:, 0, None])(cat_fea_in)
-    brand_id = Lambda(lambda x: x[:, 1, None])(cat_fea_in)
-    store_embed = Embedding(max_grain1_id + 1, ceil(log(max_grain1_id + 1)), input_length=1)(store_id)
-    brand_embed = Embedding(max_grain2_id + 1, ceil(log(max_grain2_id + 1)), input_length=1)(brand_id)
+    cat_flatten = []
+    for i, m in enumerate(max_cat_id):
+        cat_fea = Lambda(lambda x: x[:, i, None])(cat_fea_in)
+        cat_fea_embed = Embedding(m + 1, ceil(log(m + 1)), input_length=1)(cat_fea)
+        cat_flatten.append(Flatten()(cat_fea_embed))
 
     # Dilated convolutional layers
     c1 = Conv1D(filters=n_filters, kernel_size=kernel_size, dilation_rate=1, padding="causal", activation="relu")(
@@ -60,11 +57,11 @@ def create_dcnn_model(
     conv_out = Flatten()(conv_out)
 
     # Concatenate with categorical features
-    x = concatenate([conv_out, Flatten()(store_embed), Flatten()(brand_embed)])
+    x = concatenate([conv_out] + cat_flatten)
     x = Dense(16, activation="relu")(x)
     output = Dense(n_outputs, activation="linear")(x)
 
-    # Define model interface, loss function, and optimizer
+    # Define model interface
     model = Model(inputs=[seq_in, cat_fea_in], outputs=output)
 
     return model
