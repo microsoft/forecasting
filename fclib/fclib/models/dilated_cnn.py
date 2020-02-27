82eac5adfc69440802e97070ed87b892b40dbe55
Ubuntu <chenhhu@microsoft.com>
2020-02-27 22:47:17 +0000

    generalized network definition

--- fclib/fclib/models/dilated_cnn.py
+++ fclib/fclib/models/dilated_cnn.py
@@ -2,8 +2,7 @@
 # Licensed under the MIT License.
 
 """
-This file contains utility functions for builing Dilated CNN model to
-solve time series forecasting problems.
+This file contains utility functions for builing Dilated CNN model to solve time series forecasting problems.
 """
 
 
@@ -13,19 +12,25 @@ from tensorflow.keras.models import Model
 
 
 def create_dcnn_model(
-    seq_len, n_dyn_fea=1, n_cat_fea=2, n_outputs=1, kernel_size=2, n_filters=3, dropout_rate=0.1, max_cat_id=[1e3, 1e3],
+    seq_len,
+    n_dyn_fea=1,
+    n_outputs=1,
+    n_dilated_layers=3,
+    kernel_size=2,
+    n_filters=3,
+    dropout_rate=0.1,
+    max_cat_id=[1e3, 1e3],
 ):
     """Create a Dilated CNN model.
 
     Args: 
         seq_len (int): Input sequence length
         n_dyn_fea (int): Number of dynamic features
-        n_cat_fea (int): Number of categorical features
         n_outputs (int): Number of outputs of the network
         kernel_size (int): Kernel size of each convolutional layer
         n_filters (int): Number of filters in each convolutional layer
         dropout_rate (float): Dropout rate in the network
-        max_cat_id (list[int]): Maximum value of the grain1 ID
+        max_cat_id (list[int]): Each entry in the list represents the maximum value of the ID of a specific categorical variable. 
 
     Returns:
         object: Keras Model object
@@ -34,25 +39,35 @@ def create_dcnn_model(
     seq_in = Input(shape=(seq_len, n_dyn_fea))
 
     # Categorical input
+    n_cat_fea = len(max_cat_id)
+    print(n_cat_fea)
     cat_fea_in = Input(shape=(n_cat_fea,), dtype="uint8")
     cat_flatten = []
     for i, m in enumerate(max_cat_id):
-        cat_fea = Lambda(lambda x: x[:, i, None])(cat_fea_in)
+        cat_fea = Lambda(lambda x, i: x[:, i, None], arguments={"i": i})(cat_fea_in)
         cat_fea_embed = Embedding(m + 1, ceil(log(m + 1)), input_length=1)(cat_fea)
         cat_flatten.append(Flatten()(cat_fea_embed))
 
     # Dilated convolutional layers
-    c1 = Conv1D(filters=n_filters, kernel_size=kernel_size, dilation_rate=1, padding="causal", activation="relu")(
-        seq_in
+    conv1d_layers = []
+    conv1d_layers.append(
+        Conv1D(filters=n_filters, kernel_size=kernel_size, dilation_rate=1, padding="causal", activation="relu")(seq_in)
     )
-    c2 = Conv1D(filters=n_filters, kernel_size=kernel_size, dilation_rate=2, padding="causal", activation="relu")(c1)
-    c3 = Conv1D(filters=n_filters, kernel_size=kernel_size, dilation_rate=4, padding="causal", activation="relu")(c2)
+    for i in range(1, n_dilated_layers):
+        conv1d_layers.append(
+            Conv1D(
+                filters=n_filters, kernel_size=kernel_size, dilation_rate=2 ** i, padding="causal", activation="relu"
+            )(conv1d_layers[i - 1])
+        )
 
     # Skip connections
-    c4 = concatenate([c1, c3])
+    if n_dilated_layers > 1:
+        c = concatenate([conv1d_layers[0], conv1d_layers[-1]])
+    else:
+        c = conv1d_layers[0]
 
     # Output of convolutional layers
-    conv_out = Conv1D(8, 1, activation="relu")(c4)
+    conv_out = Conv1D(8, 1, activation="relu")(c)
     conv_out = Dropout(dropout_rate)(conv_out)
     conv_out = Flatten()(conv_out)
 
