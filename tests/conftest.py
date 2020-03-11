617784831c48aab54eb316c9bf85fd00258f71dd
Vanja Paunic <15053814+vapaunic@users.noreply.github.com>
2020-03-11 10:19:48 +0000

    fixing notebook tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,8 @@ def notebooks():
     """
     repo_path = git_repo_path()
     examples_path = os.path.join(repo_path, "examples")
-    quick_start_path = os.path.join(examples_path, "00_quick_start")
+    usecase_path = os.path.join(examples_path, "oj_retail", "python")
+    quick_start_path = os.path.join(usecase_path, "00_quick_start")
 
     # Path for the notebooks
     paths = {"lightgbm_quick_start": os.path.join(quick_start_path, "lightgbm_single_round.ipynb")}
