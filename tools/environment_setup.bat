bc3b46342f70f81f8718985e64381e3db52312f6
Ubuntu <chenhhu@microsoft.com>
2020-03-17 03:44:57 +0000

    initial test

---
+++ tools/environment_setup.bat
@@ -0,0 +1,6 @@
+: Follow instructions in this link 
+: https://docs.conda.io/projects/conda/en/latest/user-guide/install/windows.html 
+: to install Miniconda first
+
+echo Update conda
+call conda update conda
