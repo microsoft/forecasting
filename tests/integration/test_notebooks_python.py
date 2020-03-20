6a247bc155ad53284b3f783060614329c74d41a0
Ubuntu <chenhhu@microsoft.com>
2020-03-20 19:09:17 +0000

    added ci test of lightgbm multi round example

--- tests/integration/test_notebooks_python.py
+++ tests/integration/test_notebooks_python.py
@@ -19,3 +19,15 @@ def test_lightgbm_quick_start(notebooks):
     assert df.shape[0] == 1
     mape = df.loc[df.name == "MAPE"]["data"][0]
     assert mape == pytest.approx(35.60, abs=ABS_TOL)
+
+
+@pytest.mark.integration
+def test_lightgbm_multi_round(notebooks):
+    notebook_path = notebooks["lightgbm_multi_round"]
+    output_notebook_path = os.path.join(os.path.dirname(notebook_path), "output.ipynb")
+    pm.execute_notebook(notebook_path, output_notebook_path, kernel_name="forecast_cpu")
+    nb = sb.read_notebook(output_notebook_path)
+    df = nb.scraps.dataframe
+    assert df.shape[0] == 1
+    mape = df.loc[df.name == "MAPE"]["data"][0]
+    assert mape == pytest.approx(36.0, abs=ABS_TOL)
