3d68d0f9c1a2de6682e60d4ec49a86ed0a6f6ed2
Hong Ooi <hongooi@microsoft.com>
2020-07-02 00:09:32 +0000

    loss fn tests

---
+++ fclib/tests/test_evaluation.py
@@ -0,0 +1,23 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import numpy as np
+import pandas as pd
+from fclib.evaluation.evaluation_utils import *
+
+
+y = np.array([1, 2, 3])
+yhat = np.array([1.1, 2.2, 3.3])
+
+
+def test_MAPE():
+    assert abs(MAPE(yhat, y) - 0.1) < 1e-5
+
+
+def test_sMAPE():
+    assert abs(sMAPE(yhat, y) - 0.04761904) < 1e-5
+
+
+def test_pinball_loss():
+    df = pd.DataFrame({'yhat': yhat, 'y': y})
+    assert all(abs(pinball_loss(df.yhat, df.y, 0.5) - pd.Series([0.05, 0.1, 0.15])) < 1e-5)
